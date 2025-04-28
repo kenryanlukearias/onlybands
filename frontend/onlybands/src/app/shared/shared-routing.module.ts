11e3b2ff8345b3ed4bae07e8d217b3afbf15449f
kenryanlukearias <studyholicprocrastinator@gmail.com>
2025-04-29 00:12:36 +0400

    Header is now responsive, need to make menu bar for burger

--- frontend/onlybands/src/app/shared/shared-routing.module.ts
+++ frontend/onlybands/src/app/shared/shared-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from '../register/login-page/login.component';
+import { LandiingComponent } from '../home/landing-section/landiing.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'landing', component: LandiingComponent },
 ];
 
 @NgModule({
