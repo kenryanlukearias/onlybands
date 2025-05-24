247752ab4596f4c4a6a8328e55381a4050a4cdec
kenryanlukearias <studyholicprocrastinator@gmail.com>
2025-05-24 23:12:15 +0400

    Fixed problems with putting header adn footer in lessons

--- frontend/onlybands/src/app/home/lessons/lessons-page.component.ts
+++ frontend/onlybands/src/app/home/lessons/lessons-page.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { SharedModule } from "../../shared/shared.module";
+import { HeaderComponent } from "../../shared/header/header.component";
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
-  selector: 'app-lessons-page',
-  imports: [],
+  selector: 'app-lessons',
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './lessons-page.component.html',
   styleUrl: './lessons-page.component.scss'
 })
